ae4a0d98b97e4059480de69479722b9d00c5c549
molim <mohade3.karbalaei@gmail.com>
2022-12-07 13:20:18 +0100

    add assest file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <main>
+        <Hero />
+      </main>
+    </>
+  );
 };
 
 export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+//the first component
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <div>
+        <div></div>
+        <div>
+          <div>
+            <h1></h1>
+            <h1></h1>
+            <button></button>
+            <div></div>
+            <div></div>
+          </div>
+          <div>
+            <img src="" alt="hero-img/img" className="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
+
+// 1 wrapper div
+// 2 container div
+// 3 gradient background
+// 4 text content -> image content
+// 5 video links
+// 6 social links
